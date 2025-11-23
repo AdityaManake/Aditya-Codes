d4512e684b15911f1b7673f76e7d522d6d5f214a
AdityaManake <adityapmanake@gmail.com>
2025-11-23 20:25:26 +0530

    Incomplete file

---
+++ ML_using_python/Data_Preprocessing/onehot_encoding.py
@@ -0,0 +1,9 @@
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+data={
+    "Name":["Aditya","Manas","Geet","Tanvi","Pranav","Sahil","Sarah"],
+    "Age":[None,22,None,21,22,20,21],
+    "Hobby":["Gaming","Coding","Coding","Gaming","Coding","Coding","Gaming"],
+    "CGPA":[8.7,None,8.3,8.4,None,9.0,8.5],
+    "Gender":["Male","Male","Female","Male","Male","Male","Female"]
+}
