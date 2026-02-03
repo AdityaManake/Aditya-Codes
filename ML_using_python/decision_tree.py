f9f4c28cbc2607776171d79d8a66adc82f606c40
AdityaManake <adityapmanake@gmail.com>
2026-02-03 23:57:22 +0530

    Updated file

--- ML_using_python/decision_tree.py
+++ ML_using_python/decision_tree.py
@@ -17,8 +17,8 @@ X["degree_n"]=le_degree.fit_transform(X["degree"])
 X=X.drop("company",axis="columns")
 X=X.drop("job",axis="columns")
 X=X.drop("degree",axis="columns")
-#company-ggl=2,abc=0,fb=1
 #job-sales=2,computer=0,business=1
+#company-ggl=2,abc=0,fb=1
 #degree-ms=1,bachelor=0
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 print(X_test)
